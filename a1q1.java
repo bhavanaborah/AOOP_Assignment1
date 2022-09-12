0a74501ce7a60d8e9c39c65b3e31e888a65276a6
bhavanaborah <63739417+bhavanaborah@users.noreply.github.com>
2022-09-12 18:41:03 +0530

    Update a1q1.java

--- a1q1.java
+++ a1q1.java
@@ -16,28 +16,28 @@ public class a1q1 {
 		Scanner sc = new Scanner(System.in);
 		//try catch block
 		try {
-			//System.out.println("Enter value of  a and b:");
-			//int a = 9;
-			//int b = 0;
-			//int  divi = a/b;
+			System.out.println("Enter value of  a and b:");
+			int a = 9;
+			int b = 0;
+			int  divi = a/b;
 			
-			/*int[] intarray = new int[4];
+			int[] intarray = new int[4];
 			intarray [0]=1;
 			intarray [1]=2;
 			intarray [2]=3;
 			intarray [3]=4;
 			intarray [4]=5;
-			*/
+			
 			
 			animal an = new animal();
 			an.setfood("meat");
 			
-		}//catch(ArithmeticException ae) { //ArithmeticException handling
-			//System.out.println("Error 1:"+ae);
-		//}
-		//catch(ArrayIndexOutOfBoundsException ao) { //ArrayIndexOutOfBounds Exception handling
-			//System.out.println("Error 2:"+ao);
-		//}
+		}catch(ArithmeticException ae) { //ArithmeticException handling
+			System.out.println("Error 1:"+ae);
+		}
+		catch(ArrayIndexOutOfBoundsException ao) { //ArrayIndexOutOfBounds Exception handling
+			System.out.println("Error 2:"+ao);
+		}
 		catch(NoSuchMethodException np) { //NoSuchMethodException handling
 			System.out.println("Error 3:"+np);
 		}
